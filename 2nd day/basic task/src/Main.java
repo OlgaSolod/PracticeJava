81d336abc0fedce690ad164eb2be70430d8f2fb2
OlgaSolod <41755983+OlgaSolod@users.noreply.github.com>
2021-10-26 22:39:23 +0300

    add basic task for 2nd day' :

---
+++ 2nd day/basic task/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main extends StringSeparator{
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String inputString = reader.readLine();
+        String inputNumber = reader.readLine();
+        int maxLength = Integer.parseInt(inputNumber);
+        getStringLength(inputString);
+        getSymbolsWithoutSpace(inputString);
+        getStringOddOrOddness(inputString);
+        compareStringLengthToMaxLength(inputString, maxLength);
+    }
+}
