b959b3dc5c14f48ea6440edef4a8fd4c1844d49e
OlgaSolod <41755983+OlgaSolod@users.noreply.github.com>
2021-10-28 13:07:53 +0300

    Refactoring task

--- 1st day/basic task/GetTimeAndHello.java
+++ 1st day/basic task/GetTimeAndHello.java
@@ -1,15 +1,20 @@
 
 import java.time.LocalTime;
 
-public class GetTimeAndHello extends TimeOfDay {
+public class GetTimeAndHello {
     private static String username = "SpottyPython";
 
     public static void main(String[] args) {
         LocalTime time = LocalTime.now();
         int current = time.getHour();
-        sayGoodNight(current,username);
-        sayGoodMorning(current,username);
-        sayGoodAfternoon(current,username);
-        sayGoodEvening(current,username);
+        if (current >= 0 && current <= 4) {
+            System.out.println("Доброй ночи, " + username);
+        } else if (current > 4 && current <= 9) {
+            System.out.println("Доброе утро, " + username);
+        } else if (current > 9 && current <= 16) {
+            System.out.println("Добрый день, " + username);
+        } else if (current > 16 && current <= 23) {
+            System.out.println("Доброй вечер, " + username);
+        }
     }
 }
