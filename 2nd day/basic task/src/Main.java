181fc60f2c453db5bbe7e75d157545fbaf69c31e
OlgaSolod <41755983+OlgaSolod@users.noreply.github.com>
2021-10-30 23:32:40 +0300

    refactoring basic task of 2nd day

--- 2nd day/basic task/src/Main.java
+++ 2nd day/basic task/src/Main.java
@@ -3,16 +3,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 
-public class Main extends StringSeparator{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String inputString = reader.readLine();
         String inputNumber = reader.readLine();
         int maxLength = Integer.parseInt(inputNumber);
-        getStringLength(inputString);
-        getSymbolsWithoutSpace(inputString);
-        getStringOddOrOddness(inputString);
-        compareStringLengthToMaxLength(inputString, maxLength);
+        System.out.println("Длина строки: " + inputString.length());
+        System.out.println("Длина строки без пробелов: " + inputString.replace(" ", "").length());
+        System.out.println("Количество символов в строке: " + (inputString.length() % 2 == 0 ? "четное" : "нечетное"));
+        if (inputString.length() > maxLength) {
+            System.out.println("Длина текста превышает длину " + maxLength + " символов");
+        }
         reader.close();
     }
 }
