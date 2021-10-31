315a60dd8f55d30de6f72d5b5da8a85235fade61
Olga <jasminess847@gmail.com>
2021-10-31 14:41:52 +0300

    add basic task of 5th day

---
+++ 5th day/basic task/src/Number.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Number {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int num = 0;
+        try {
+            num = Integer.parseInt(reader.readLine());
+            if (num < 0) {
+                System.out.println("Вы ввели отрицательное число");
+            } else if (num > 99) {
+                System.out.println("Вы ввели трехзначное число");
+            }
+        } catch (IOException e) {
+            System.out.println("Вы ввели не число");
+        }
+        System.out.println(checkTwoFigure(num));
+    }
+
+    static boolean checkTwoFigure(int number) {
+        int secondDigit = number % 10;
+        int firstDigit = (number - secondDigit) / 10;
+        boolean sumOfDigits = (secondDigit + firstDigit == 10);
+        return (secondDigit == firstDigit || sumOfDigits);
+
+
+    }
+
+}
